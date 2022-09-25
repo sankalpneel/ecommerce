d395b4088f3a7dc474481e4f3979719119c622c7
Sankalp <neel007.sk@gmail.com>
2022-09-25 19:49:17 +0530

    first

---
+++ database/databasetup.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://sankalp:sankalp@ecommerce.etkfx92.mongodb.net/?retryWrites=true&w=majority"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed  to connect to mongoDB")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to mongodb")
+	return client
+
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerece").Collection(collectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productCollection *mongo.Collection = client.Database("Ecommerece").Collection(collectionName)
+	return productCollection
+}
